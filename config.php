1230450021680e4ea97ff79971b9b10308e4e3f2
RichFraYoli <novaproget@gmail.com>
2020-01-07 14:59:57 +0100

    Update config.php

--- config.php
+++ config.php
@@ -3,16 +3,16 @@
 
      //
      
-public function scan_dir($dir){
+public function scan_dir_order_by_last_access($dirarray,$dir){
 
-            foreach( $listDir as $val){  if(is_dir($dir.$val)){
-$stat = stat($dir.$val);
-var_dump($stat['atime']);
-echo "<br>";
-$outxz[]=rsort($stat['atime']); //  qui non ottengo nulla tutto bool null
-var_dump($stat['mtime']);
+foreach( $dirarray as $val){  if(is_dir($dir.$val)){
+    $stat = stat($dir.$val);
+      var_dump($stat['atime']);//ok
+       echo "<br>";
+           $outxz[]=rsort($stat['atime']); //  qui non ottengo nulla tutto bool null
+       var_dump($stat['mtime']); //ok 
 
 
-}  } 
+       }     } 
 
 }
