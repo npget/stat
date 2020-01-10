10493b529bd5bcbc89ea397a0b8f4f70e49f50df
RichFraYoli <novaproget@gmail.com>
2020-01-10 18:13:14 +0100

    Update config.php

--- config.php
+++ config.php
@@ -6,7 +6,12 @@
 public function scan_dir_order_by_last_access($listDir,$dir){
  $btndir=null;$btnfile=null;
  foreach( $listDir as $val){  if(is_dir($dir.$val)){  $stat = stat($dir.$val);
-                 $arraystat[$val][]=$stat['mtime'];      }    }
+                 $arraystat[$val]=$stat['mtime']; 
+     // SE PASSO dentro $arraystat[$val] un valore  numerico e solo numerico  
+     // OVVIAMENTE ME LA SCARTA e mi ARRAY IN CHIAVE  ritorna [0]=> int(1578674338)
+   
+     
+     }    }
                  
 array_multisort($arraystat,SORT_DESC,$arraystat);
 //var_dump($arraystat);
@@ -19,8 +24,7 @@ array_multisort($arraystat,SORT_DESC,$arraystat);
        
              
               $btndir.= $this->list_dir_explorer($dir, $val); }
-             // SE PASSO dentro val un valore  numerico e solo numerico  
-     // OVVIAMENTE ME LA SCARTA e non mi torna mi ritorna [0]=> int(1578674338)
+            
              
           }
   var_dump($arraystat);
