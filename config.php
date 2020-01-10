992dc7d394c803b20c961ebb4b8a7a738e5df0fa
RichFraYoli <novaproget@gmail.com>
2020-01-10 12:59:40 +0100

    Update config.php

--- config.php
+++ config.php
@@ -6,13 +6,34 @@
 public function scan_dir_order_by_last_access($dirarray,$dir){
 
 foreach( $dirarray as $val){  if(is_dir($dir.$val)){
-    $stat = stat($dir.$val);
-      var_dump($stat['atime']);//ok
-       echo "<br>";
-           $outxz[]=rsort($stat['atime']); //  qui non ottengo nulla tutto bool null
-       var_dump($stat['mtime']); //ok 
+$stat = stat($dir.$val);
+
+$arraystat[$val]=$stat['mtime'];
 
 
-       }     } 
+} 
+}
+array_multisort($arraystat,SORT_DESC);
+var_dump($arraystat);
+     
+    $c=0;
+        foreach( $listDir as $val){
+          $c++;
+       
+            if(is_file($dir.$val)){
+$btnfile.=$this->list_file_explorer($dir,$val);
+             }
+             if(is_dir($dir.$val)){
+        
+ $btndir.= $this->list_dir_explorer($dir,$arraystat);
+             
+             
+             }
+             
+             
+             } 
+     
+     
+     
 
 }
