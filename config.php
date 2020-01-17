8c0c5b1595d1799365b062bb231f4695c23d5aed
RichFraYoli <novaproget@gmail.com>
2020-01-17 09:58:37 +0100

    Update config.php

--- config.php
+++ config.php
@@ -4,14 +4,15 @@
      //
      
 public function scan_dir_order_by_last_access($listDir,$dir){
- $btndir=null;$btnfile=null;
- foreach( $listDir as $val){  if(is_dir($dir.$val)){  $stat = stat($dir.$val);
-                 $arraystat[$val]=$stat['mtime']; 
-      $arrayval[]=$val;
-     
-   
-     
-     }    }
+ 
+     $btndir=null;$btnfile=null;
+ foreach( $listDir as $val){  
+     if(is_dir($dir.$val)){ 
+                            $stat = stat($dir.$val);
+                            $arraystat[$val]=$stat['mtime']; 
+                            $arrayval[]=$val;
+                           }    
+                          }
                  
 array_multisort($arraystat,SORT_DESC); // non so come fare il sort 
      // in modo che mi esca una lista crescente per creazione dir ---
@@ -20,17 +21,14 @@ array_multisort($arraystat,SORT_DESC); // non so come fare il sort
 
   foreach( $arraystat as $val => $key){
             
-            if(is_dir($dir.$arrayval[$val])){
-             
-         
-             
-              $btndir.= $this->list_dir_explorer($dir,$arrayval[$val]); }
+     if(is_dir($dir.$arrayval[$val])){
+            
+         $btndir.= $this->list_dir_explorer($dir,$arrayval[$val]); 
+     }
             
              
-          }
+  }
+     
   var_dump($arraystat);
      
-     
-     
-
-}
+    }
